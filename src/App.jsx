1b9e8c50cc2b0e26b891145256c7a6f1b0fa3612
Micah Anderson <micah.anderson@dci-student.org>
2023-06-08 00:23:16 +0200

    add list and add delete buttonn

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,23 @@ export default function App() {
       <button className="btn">Add</button>
     </form>
     <h1 className="header">To Do List</h1>
+    <ul className="list">
+      <li>
+        <label>
+          <input type="checkbox" />
+          Item 1
+        </label>
+        <button className="btn btn-danger">Destroy</button>
+      </li>
+
+      <li>
+        <label>
+          <input type="checkbox" />
+          Item 2
+        </label>
+        <button className="btn btn-danger">Destroy</button>
+      </li>
+    </ul>
     </>
   )
 }
